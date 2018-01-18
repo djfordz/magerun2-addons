f38226204cd1a646aa083c65b1cdc083a7480a92
David Ford <djfordz@gmail.com>
2018-01-18 10:01:44 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Magerun2 Addons
+
+Send customer email to check if email is working on server
+
+
+Create ~/.n98-magerun/modules/ if it doesn't already exist.
+```
+mkdir -p ~/.n98-magerun2/modules/
+```
+
+Clone the magerun-addons repository in there
+```
+cd ~/.n98-magerun2/modules/ && git clone https://github.com/djfordz/magerun2-addons.git df-addons2
+```
+
+Run Command
+
+n98-magerun2 customer:sendemail <your email> 
+
+
---
+++ src/Magefoo/Magento/Command/Customer/SendEmailCommand.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Magefoo\Magento\Command\Customer;
+
+use Magento\Framework\App\State;
+use Magento\Framework\App\Area;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SendEmailCommand extends \N98\Magento\Command\Customer\AbstractCustomerCommand
+{
+    protected $_state;
+    protected $_scopeConfig;
+    protected $_storeManager;
+    protected $inlineTranslation;
+    protected $_transportBuilder;
+    protected $_config;
+    protected $_template;
+
+    protected function configure()
+    {
+        $this
+            ->setName('customer:sendemail')
+            ->addArgument('email', InputArgument::REQUIRED, 'Email')
+            ->setDescription('Send Random Order Email through Queue, to test email system');
+    }
+
+    public function inject(
+        \Magento\Framework\App\Helper\Context $context,
+        \Magento\Framework\App\State $state,
+        \Magento\Framework\Mail\Template\TransportBuilder $transportBuilder,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Framework\Translate\Inline\StateInterface $inlineTranslation,
+        \Magento\Framework\App\Config $config
+    ) {
+        $this->_scopeConfig = $context;
+        $this->_state = $state;
+        $this->_storeManager = $storeManager;
+        $this->inlineTranslation = $inlineTranslation;
+        $this->_transportBuilder = $transportBuilder;
+        $this->_config = $config;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // Detect and Init Magento
+        $this->detectMagento($output, true);
+        if(!$this->initMagento()) {
+            return;
+        }
+
+        $this->_state->setAreaCode(\Magento\Framework\App\Area::AREA_FRONTEND);
+
+        $transport = $this->_transportBuilder->setTemplateIdentifier(
+            'sales_email_order_template'
+        )->setTemplateOptions(
+            array('area' => \Magento\Framework\App\Area::AREA_FRONTEND, 'store' => 0)
+        )->setTemplateVars(
+            array('user' => $input->getArgument('email'), 'store' => $this->_storeManager->getStore(0))
+        )->setFrom(
+            array('name' => 'test', 'email' => $this->_config->getValue('trans_email/ident_sales/email'))
+        )->addTo(
+            $input->getArgument('email'), 'test'
+        )->getTransport();
+
+        $this->inlineTranslation->suspend();
+
+        try {
+            $transport->sendMessage();
+        } catch(\Exception $e) {
+            echo $e->getMessage();
+        }
+        
+
+        $this->inlineTranslation->resume();
+
+        $output->writeln("email successfully sent to" . $input->getArgument('email'));
+
+    }
+}
