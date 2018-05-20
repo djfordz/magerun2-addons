307af6615ca3b0a44d233363d572c4e3e06d640d
David Ford <djfordz@gmail.com>
2018-05-20 10:47:50 -0500

    fixed area code for customer:sendemail

--- src/Magefoo/Magento/Command/Customer/SendEmailCommand.php
+++ src/Magefoo/Magento/Command/Customer/SendEmailCommand.php
@@ -2,23 +2,31 @@
 
 namespace Magefoo\Magento\Command\Customer;
 
-use Magento\Framework\App\State;
-use Magento\Framework\App\Area;
-
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class SendEmailCommand extends \N98\Magento\Command\Customer\AbstractCustomerCommand
+use N98\Util\Console\Helper\InjectionHelper;
+use N98\Magento\Command\Customer\AbstractCustomerCommand;
+
+
+class SendEmailCommand extends AbstractCustomerCommand
 {
-    protected $_state;
+
+    const XML_PATH_EMAIL_ORDER_TEMPLATE = 'sales_email_order_template';
+    const XML_PATH_EMAIL_IDENTITY = 'trans_email/ident_sales/email';
+
+    protected $_context;
+    protected $_registry;
+    protected $_appState;
     protected $_scopeConfig;
-    protected $_storeManager;
+    protected $_storeManagerFactory;
     protected $inlineTranslation;
     protected $_transportBuilder;
     protected $_config;
     protected $_template;
     protected $productMetadata;
+    protected $_website;
 
     protected function configure()
     {
@@ -26,18 +34,27 @@ class SendEmailCommand extends \N98\Magento\Command\Customer\AbstractCustomerCom
             ->setName('customer:sendemail')
             ->addArgument('email', InputArgument::REQUIRED, 'Email')
             ->setDescription('Send Random Order Email through Queue, to test email system');
+        parent::configure();
     }
 
     public function inject(
-        \Magento\Framework\App\State $state,
-        \Magento\Framework\App\ProductMetadataInterface $productMetadata,
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\View\LayoutFactory $layoutFactory,
+        \Magento\Framework\App\State $appState,
+        \Magento\Store\Model\App\Emulation $appEmulation,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
         \Magento\Framework\Mail\Template\TransportBuilder $transportBuilder,
         \Magento\Store\Model\StoreManagerInterface $storeManager,
         \Magento\Framework\Translate\Inline\StateInterface $inlineTranslation,
         \Magento\Framework\App\Config $config
     ) {
-        $this->_state = $state;
-        $this->productMetadata = $productMetadata;
+        $this->_context = $context;
+        $this->registry = $registry;
+        $this->_layoutFactory = $layoutFactory;
+        $this->_appState = $appState;
+        $this->_appEmulation = $appEmulation;
+        $this->_scopeConfig = $scopeConfig;
         $this->_storeManager = $storeManager;
         $this->inlineTranslation = $inlineTranslation;
         $this->_transportBuilder = $transportBuilder;
@@ -46,48 +63,132 @@ class SendEmailCommand extends \N98\Magento\Command\Customer\AbstractCustomerCom
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        
+        $this->_appState->setAreaCode(\Magento\Framework\App\Area::AREA_FRONTEND);
+
         // Detect and Init Magento
         $this->detectMagento($output, true);
-        if(!$this->initMagento()) {
+        if (!$this->initMagento()) {
             return;
         }
 
-        if (version_compare($this->productMetadata->getVersion(), '2.1.5', '<') || (version_compare($this->productMetadata->getVersion(), '2.2.0', '>') && version_compare($this->productMetadata->getVersion(), '2.2.3', '<'))) {
-            $output->writeln('Not compatible with this version of Magento, please upgrade to latest version.');
-            return;
-        }
-        $this->_state->setAreaCode(\Magento\Framework\App\Area::AREA_FRONTEND);
+        $this->_appState->emulateAreaCode(
+            \Magento\Framework\App\Area::AREA_FRONTEND, function () use (
+                $input
+            ) {
 
-        $stores = $this->_storeManager->getStores();
+                $websites = $this->_storeManager->getWebsites();
+                foreach ($websites as $website) {
+                    $this->_website = $website;
+                    break;
+                }
 
-        foreach ($stores as $store) {
-            $storeCode = $store->getCode();
-            break;
-        }
+                $store = $this->_website->getDefaultStore();
 
-        $transport = $this->_transportBuilder->setTemplateIdentifier(
-            'sales_email_order_template'
-        )->setTemplateOptions(
-            array('area' => \Magento\Framework\App\Area::AREA_FRONTEND, 'store' => $storeCode)
-        )->setTemplateVars(
-            array('user' => $input->getArgument('email'), 'store' => $this->_storeManager->getStore($storeCode))
-        )->setFrom(
-            array('name' => 'test', 'email' => $this->_config->getValue('trans_email/ident_sales/email'))
-        )->addTo(
-            $input->getArgument('email'), 'test'
-        )->getTransport();
+                $storeId = $store->getId();
 
-        $this->inlineTranslation->suspend();
+                $this->_appEmulation->startEnvironmentEmulation($storeId);
 
-        try {
-            $transport->sendMessage();
-        } catch(\Exception $e) {
-            echo "there was an error: " . $e->getMessage();
-        }
+                print_r("sending email...\n");
+                $layout = $this->_layoutFactory->create(['cacheable' => false]);
+                $layout->getUpdate()->addHandle('test')->load();
+
+                $block = $layout->createBlock(\Magento\Cms\Block\Block::class);
+
+                $alertGrid = $this->_appState->emulateAreaCode(
+                    \Magento\Framework\App\Area::AREA_FRONTEND,
+                    [$block, 'toHtml']
+                );
+
+                $this->_appEmulation->stopEnvironmentEmulation();
+
+                $transport = $this->_transportBuilder->setTemplateIdentifier(
+                    self::XML_PATH_EMAIL_ORDER_TEMPLATE
+                )->setTemplateOptions(
+                    [
+                        'area' => \Magento\Framework\App\Area::AREA_FRONTEND,
+                        'store' => $storeId,
+                    ]
+                )->setTemplateVars(
+                     [
+                        'customerName' => 'test',
+                        'alertGrid' => $alertGrid,
+                    ] 
+                )->setFrom(
+                    $this->senderEmail(null, $storeId)
+                )->addTo(
+                    $input->getArgument('email'), 'test'
+                )->getTransport();
+
+                $this->inlineTranslation->suspend();
+
+                $transport->sendMessage();
+                
+                $this->inlineTranslation->resume();
+            }
+        );
         
-        $this->inlineTranslation->resume();
-
         $output->writeln("email successfully sent to: " . $input->getArgument('email'));
-
     }
+
+    public function senderEmail($type = null, $storeId = null) {
+        $sender['email'] = $this->_scopeConfig->getValue(
+            self::XML_PATH_EMAIL_IDENTITY,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+            $storeId
+        );
+        $sender['name'] = 'admin';
+
+        return $sender;
+    }
+
+    public function emailTemplate($storeId = null)
+    {
+        $templateId = $this->_scopeConfig->getValue(
+            self::XML_PATH_EMAIL_ORDER_TEMPLATE,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+            $storeId
+        );
+
+        return $templateId;
+    }
+
+    public function emulateAreaCallback()
+    {
+        $skipParams = ['handle', 'area'];
+
+        /** @var $layout \Magento\Framework\View\LayoutInterface */
+        $layout = $this->_layoutFactory->create(['cacheable' => false]);
+        $layout->getUpdate()->addHandle('test')->load();
+
+        $layout->generateXml();
+        $layout->generateElements();
+
+        $rootBlock = false;
+        foreach ($layout->getAllBlocks() as $block) {
+            /* @var $block \Magento\Framework\View\Element\AbstractBlock */
+            if (!$block->getParentBlock() && !$rootBlock) {
+                $rootBlock = $block;
+            }
+            foreach ($this->_directiveParams as $k => $v) {
+                if (in_array($k, $skipParams)) {
+                    continue;
+                }
+                $block->setDataUsingMethod($k, $v);
+            }
+        }
+
+        /**
+         * Add root block to output
+         */
+        if ($rootBlock) {
+            $layout->addOutputElement($rootBlock->getNameInLayout());
+        }
+
+        $result = $layout->getOutput();
+        $layout->__destruct();
+        // To overcome bug with SimpleXML memory leak (https://bugs.php.net/bug.php?id=62468)
+        return $result;
+    }
+
 }
